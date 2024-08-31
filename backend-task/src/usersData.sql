d1338bfd6694815aeb55cef4c0b7af14d32a666f
c-l-scholl <cscholl@macalester.edu>
2024-08-31 14:06:16 -0500

    added values into db

--- backend-task/src/usersData.sql
+++ backend-task/src/usersData.sql
@@ -1,21 +1,23 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users
 (
-	user_id							VARCHAR(80) NOT NULL,
-	user_firstName			VARCHAR(32) NOT NULL,
-	user_lastName 			VARCHAR(32) NOT NULL,
-	user_email					VARCHAR(40) NOT NULL,
-	created_at 					TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (user_id)
+    user_id              VARCHAR(80) NOT NULL,
+    user_firstName       VARCHAR(32) NOT NULL,
+    user_lastName        VARCHAR(32) NOT NULL,
+    user_email           VARCHAR(40) NOT NULL,
+    created_at           TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id)
 );
 
 INSERT INTO users (user_id, user_firstName, user_lastName, user_email)
-	VALUES ("1", "Camden", "Scholl", "camden@scholl.com");
+    VALUES ('1345234532542', 'Camden', 'Scholl', 'camden@scholl.com');
 
 INSERT INTO users (user_id, user_firstName, user_lastName, user_email)
-	VALUES ("2", "Cotton Eye", "Joe", "cottoneye@joe.com");
+    VALUES ('2324523642', 'Cotton Eye', 'Joe', 'cottoneye@joe.com');
 
 INSERT INTO users (user_id, user_firstName, user_lastName, user_email)
-	VALUES ("3", "Tim", "Apple", "tim@apple.com");
+    VALUES ('32345324', 'Tim', 'Apple', 'tim@apple.com');
 
 INSERT INTO users (user_id, user_firstName, user_lastName, user_email)
-	VALUES ("4", "Billy", "Beane", "billy@beane.com");
+    VALUES ('43463246', 'Billy', 'Beane', 'billy@beane.com');
