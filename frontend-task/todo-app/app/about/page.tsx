457b113ca3f2407010c1c1ac678818142c25bcc4
Camden Scholl <89877319+c-l-scholl@users.noreply.github.com>
2024-09-04 09:40:59 -0500

    trying animation

--- frontend-task/todo-app/app/about/page.tsx
+++ frontend-task/todo-app/app/about/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import "./about.css";
+
 const About = () => {
-  return (
-    <>
-			hi
-    </>
-  );
-}
+	return (
+		<div className="container">
+			<div className="card">
+				<div className="inner">
+					<h2 className="about-title">About</h2>
+					<p>Here is my second page</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default About;
